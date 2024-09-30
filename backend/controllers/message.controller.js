21e3a8c67899763dc0dca964227c7df531d30161
anurag16067 <ui22cs09@iiitsurat.ac.in>
2024-09-30 19:08:33 +0530

    changes in message controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,4 +1,5 @@
-import Conversation from "../models/conversation.model";
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
 export const sendMessage = async (req, res) => {
     // console.log("message sent", req.params.id);
@@ -6,16 +7,61 @@ export const sendMessage = async (req, res) => {
     const {id: receiverId} = req.params; //const id = req.params.id; 
     const senderId = req.user._id;
 
-    let conversion = await Conversation.findOne({
-        participants: {senderId, receiverId},
+    //finding conversion between above two user
+    let conversation = await Conversation.findOne({
+        participants: { $all: [senderId, receiverId]},
+    })
+    //for first time conversion will be empty so we need to create a conversion
+    if(!conversation){
+        conversation = await Conversation.create({
+            participants: [senderId, receiverId],
+        })
+    }
+// create message that is comming from req.body;
+    const newMessage = new Message({
+        senderId,                // senderId: senderId,
+        receiverId,                // receiverId: receiverId,
+        message,               // message: message,
     })
 
+    if(newMessage){
+        conversation.messages.push(newMessage._id);
+    }
 
+    //SOCKET IO FUNCTIONALITY WILL GO HERE
+
+    // await conversation.save();
+    // await newMessage.save();
+    
+    //optimise code to save both conversion and newMessage
+    //This will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    res.status(201).json(newMessage);
 
     try {
         
     } catch (error) {
         console.log("Error in SendMessage controller: ", error.message);
+        res.status(500).json({error:"Internal server error"});
+    }
+}
+
+export const getMessage = async (req, res) => {
+    try {
+        const {id:userToChatId} = req.params;
+        const senderId = req.user._id;
+
+        const conversion = await Conversation.findOne({
+            participants: { $all : [senderId, userToChatId]},
+        }).populate("messages"); //populate used to get object. not reference but actual message
+        
+        if(!conversion) return res.status(200).json([]);
+        const messages = conversion.messages;
+
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getMessage controller: ", error.message);
         res.status(500).json({error:"Internal server error"});
     }
 }
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessage, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 const router = express.Router();
 
+router.get("/:id", protectRoute, getMessage)
 router.post("/send/:id", protectRoute, sendMessage);//before run sendMessage we check user is login or not
 
 export default router;
